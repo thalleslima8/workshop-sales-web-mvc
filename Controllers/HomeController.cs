ca8847a6460116d1b15f408a8628f21e8ee12cd7
Thalles Lima <thalleslima8@gmail.com>
2020-01-10 14:52:28 -0200

    Deletando Department e View Departments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers {
     public class HomeController : Controller {
