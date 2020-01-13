bcce2549a9914426773c063199f2dcdd92081279
Thalles Lima <thalleslima8@gmail.com>
2020-01-13 15:29:57 -0200

    MySQL adaptacao e primeira migracao

--- Startup.cs
+++ Startup.cs
@@ -11,27 +11,36 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 
-namespace SalesWebMVC {
-    public class Startup {
-        public Startup(IConfiguration configuration) {
+namespace SalesWebMVC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.AddControllersWithViews();
 
-    services.AddDbContext<SalesWebMVCContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("SalesWebMVCContext")));
+            services.AddDbContext<SalesWebMVCContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment()) {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
-            } else {
+            }
+            else
+            {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
@@ -43,7 +52,8 @@ namespace SalesWebMVC {
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => {
+            app.UseEndpoints(endpoints =>
+            {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
