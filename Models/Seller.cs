95c3ea3682edbd9d9a466a0a3219b9cacd4db296
Thalles Lima <thalleslima8@gmail.com>
2020-01-14 11:24:05 -0300

    Outras entidades e segunda migracao

---
+++ Models/Seller.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Department Departament { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+
+        public Seller()
+        {
+
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+
+        public void addSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void removeSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double totalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
